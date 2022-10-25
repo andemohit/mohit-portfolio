54ad92c68c6fad0ab14069365fafcb23b4e709e0
Mohit Ande <andemohit@gmail.com>
2022-10-25 17:03:29 +0530

    project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './public/**/*.html'],
+  theme: {
+    extend: {
+      transitionDuration: {
+        0: '0ms',
+        2000: '2000ms',
+        4000: '4000ms',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+    display: ['responsive', 'group-hover', 'group-focus'],
+  },
+  plugins: [],
+};
