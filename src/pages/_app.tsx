3d2319b6ef95a8381d23d8dcc17d3ae92ab4edbe
Mohit Ande <andemohit@gmail.com>
2022-11-04 23:40:24 +0530

    feat: added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '../../styles/globals.scss';
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
